4507737514926925c65cc340479938b61e397077
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2015-03-05 16:14:05 +0100

    Added dlog to urls.

--- pyvo_blog/pyvo_blog/urls.py
+++ pyvo_blog/pyvo_blog/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pyvo_blog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = [
+    url(r'^$', 'dlog.views.home', name='home'),
+    url(r'^blog/', include('dlog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+]
